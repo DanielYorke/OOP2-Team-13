9057f4a4208fcc3338dce9323d95a4fc049b99f1
AnuRam34 <100068619+AnuRam34@users.noreply.github.com>
2023-11-19 15:32:22 -0500

    Update PdfGenerator.java

--- OOPII-Team13-Project-main/tester/src/main/java/com/project/tester/PdfGenerator.java
+++ OOPII-Team13-Project-main/tester/src/main/java/com/project/tester/PdfGenerator.java
@@ -1,38 +1,50 @@
 package com.project.tester;
 
-import java.io.*;
 import org.apache.pdfbox.pdmodel.PDDocument; 
 import org.apache.pdfbox.pdmodel.PDPage; 
 import org.apache.pdfbox.pdmodel.PDPageContentStream; 
 import org.apache.pdfbox.pdmodel.font.PDType1Font;  
- 
-public class PdfGenerator {
-    public void createPDF(String pdfInformation) {
-        int numPages = 5;
 
-        try{
-                PDDocument document = new PDDocument();
-                for (int i = 1; i <= numPages; i++) 
-                {
-                    PDPage page = new PDPage();
-                    document.addPage(page);
-                    PDPageContentStream contentStream = new PDPageContentStream(document, page);
-                    contentStream.beginText();
-                    contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
-                    contentStream.newLineAtOffset(100, 700);
-                    contentStream.showText(pdfInformation+i);
-                    contentStream.newLineAtOffset(400, -650);
-                    contentStream.showText("page "+i);
-                    contentStream.endText();
-                    contentStream.close();
-                }
-                document.save("feedback.pdf");
-                document.close(); 
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class PdfGenerator {
+    public void createPDF(ArrayList<String> pdfInformation) {
+
+        try {
+            ArrayList<String> pdfContents = new ArrayList<String>();
+            pdfContents = pdfInformation;
+
+            PDDocument pdfDocument = new PDDocument();
+          
+            PDPage page = new PDPage();
+            pdfDocument.addPage(page);
+
+            PDPageContentStream contentStream = new PDPageContentStream(pdfDocument, page);
+
+            contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
+
+            contentStream.beginText();
+            contentStream.newLineAtOffset(90, 710);
+
+            for(String line: pdfContents){
+                contentStream.showText(line);
+                contentStream.newLineAtOffset(0, -15);
             }
-            catch(IOException e){
-                e.printStackTrace();
-            }    
-   
+            
+            contentStream.endText();
+
+            contentStream.close();
+
+            pdfDocument.save("Student-Feedback.pdf");
+
+            pdfDocument.close();
+
+            System.out.println("PDF created successfully with Times Roman font.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         
     }
 }
