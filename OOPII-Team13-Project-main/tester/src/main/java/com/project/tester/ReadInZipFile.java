8e285b428c5b94eae07f44c03c4b9712832b4186
AnuRam34 <100068619+AnuRam34@users.noreply.github.com>
2023-11-20 20:19:58 -0400

    Add files via upload
    Class File that reads in the zip file

---
+++ OOPII-Team13-Project-main/tester/src/main/java/com/project/tester/ReadInZipFile.java
@@ -0,0 +1,58 @@
+package com.project.tester;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class ReadInZipFile {
+    
+    //extractAndProcessStudentSubmissions - this method is used over in app
+    public static void extractAndProcessStudentSubmissions(String zipFileName, String outputFolder) throws IOException {
+        byte[] buffer = new byte[1024];
+        File outputDirectory = new File(outputFolder);
+        if (outputDirectory.exists()) {
+            System.out.println("Files have already been extracted. Skipping extraction.");
+            return;
+        }
+
+        try (ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(zipFileName))) {
+            ZipEntry zipEntry;
+
+            while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+                String entryName = zipEntry.getName();
+                String outputPath = outputFolder + File.separator + entryName;
+
+                File entryFile = new File(outputPath);
+
+                if (zipEntry.isDirectory()) {
+                    entryFile.mkdirs();
+                } else {
+                    new File(entryFile.getParent()).mkdirs();
+
+                    try (FileOutputStream fos = new FileOutputStream(entryFile)) {
+                        int length;
+                        while ((length = zipInputStream.read(buffer)) > 0) {
+                            fos.write(buffer, 0, length);
+                        }
+                    }
+                }
+                if (entryName.endsWith(".zip")) {
+                }
+            }
+        }
+    }
+
+    public static void processStudentSubmission(String submissionPath) {
+        File submissionFile = new File(submissionPath);
+        if (submissionFile.isFile() && submissionPath.endsWith(".zip")) {
+            try {
+                extractAndProcessStudentSubmissions(submissionPath, "studentSubmission");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
