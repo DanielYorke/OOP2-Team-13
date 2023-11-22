5443f89ee2207e0f98fe5c71903b76ae85c1e6f5
AnuRam34 <100068619+AnuRam34@users.noreply.github.com>
2023-11-22 12:04:10 -0500

    Update JaccardEvaluator.java
    Commit did not push properly from codespaces created from main branch. Commit was also late due to Wi-Fi issues

--- OOPII-Team13-Project-main/tester/src/main/java/com/project/tester/JaccardEvaluator.java
+++ OOPII-Team13-Project-main/tester/src/main/java/com/project/tester/JaccardEvaluator.java
@@ -2,8 +2,8 @@ package com.project.tester;
 
 public class JaccardEvaluator extends ClassEvaluation{
     
-    public JaccardEvaluator(int currentSimilarityMetric, String file1, String file2, String classTestName){
-        super(currentSimilarityMetric, file1, file2, classTestName);
+    public JaccardEvaluator(int currentSimilarityMetric, String file1, String file2){
+        super(currentSimilarityMetric, file1, file2);
     }
     
     public void setEvaluator(){
